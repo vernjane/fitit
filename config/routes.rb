95ded22baf23f575647026134f2734783f2b9b8d
vernjane <vern.jane@gmail.com>
2018-04-27 15:05:31 -0400

    Still doesn't see /robots.txt. Let's try this, damn it

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "welcome_page#welcome"
-  get '/robots.:format' => 'welcome_page#robots'
+  get '/robots.txt' => 'welcome_page#robots'
 end
