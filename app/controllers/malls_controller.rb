2ced529a57bdf7a5e1f01920731a101e89d79b48
vernjane <vern.jane@gmail.com>
2018-05-09 14:36:58 -0400

    After they search by postal code, we have 3 things to put in the order: city, province, postal. Yay, let's record them

--- app/controllers/malls_controller.rb
+++ app/controllers/malls_controller.rb
@@ -1,9 +1,21 @@
 class MallsController < ApplicationController
   def index
+      # that is actually supposed to be postal code
+      # it was done this way because, you know, what if
+      # they enter street or something even though it
+      # specifically says "Postal Code" in the search field
       location = params[:location]
       SearchedLocation.create(location:location, user: current_user)
       
       @location = GEOCODER.geocode(location)
+
+      @city = @location.city
+      @province = @location.province
+      @postal = @location.zip
+
+      order = current_user.orders.last
+      order.update(city: @city, province: @province, postal: @postal)
+
       @malls = Mall.find_by_address(params[:location])
       redirect_to root_path if @malls.nil?
   end
