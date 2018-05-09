defbe53d6950ec942f0e702127cef30c07c7b94d
vernjane <vern.jane@gmail.com>
2018-05-09 17:30:17 -0400

    Hopefully this will fix mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.perform_deliveries = true 
 
   config.action_mailer.perform_caching = false
 
