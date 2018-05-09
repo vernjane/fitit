66c7ffd856e2d2c8796c9e5f20a8034b491dbd09
vernjane <vern.jane@gmail.com>
2018-05-09 15:35:08 -0400

    'Place order' button at the end of items list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
     def new
+        ## view calls 'items_list' partial 
         order_id = params[:order_id]
         @order=current_user.orders.find(order_id)
         if @order.nil?
@@ -8,7 +9,7 @@ class ItemsController < ApplicationController
 
         @item = @order.items.new
 
-        # this is needed to show all items already added to the order
+        # this is needed b/c the view calls "items_list" partial
         @items = @order.items
         
         # Side note:
@@ -16,7 +17,27 @@ class ItemsController < ApplicationController
         # item.id.nil? This is needed b/c aparently that 
         # @order.items.new is being included in the order.items list.
         # Fuck knows why, and, tbh, who cares.
+
+        # this is needed b/c the view calls "items_list" partial
+        @place_order_button = is_place_order_button_needed(@items)
+    end
+
+    def is_place_order_button_needed(items)
+        # no items at all? 
+        # the button is not needed
+        if items.length < 1
+            return false
+        end
         
+        # only one item in the order? 
+        # check if it is this item.new unreal-real item (it's id will be nil)
+        if items.length == 1
+            return !items.last.id.nil?
+        end
+
+        # more than 1 item? 
+        # obviously we need the "place order" button!
+        return true
     end
 
     def edit
@@ -36,9 +57,13 @@ class ItemsController < ApplicationController
     end
 
     def index
+        ## view calls 'items_list' partial 
         order_id = params[:order_id]
         @order = current_user.orders.find(order_id)
         @items = @order.items
+
+        # this is needed b/c the view calls "items_list" partial
+        @place_order_button = is_place_order_button_needed(@items)
     end
 
     def update
@@ -58,3 +83,11 @@ class ItemsController < ApplicationController
         params.require(:item).permit(:description, :color, :size)
     end
 end
+
+# NOTES:
+
+=begin
+    items_list partial is called from index and new
+    items_list needs '@items' and '@place_order_button'
+    @place_order_button: run is_place_order_button_needed(items)
+=end
