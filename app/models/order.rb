3a73d44b045634b87a5693ba7565d6de1595aff2
vernjane <vern.jane@gmail.com>
2018-05-09 16:37:02 -0400

    Include items_list in order show. Format order details in show a bit

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,20 @@ class Order < ApplicationRecord
     belongs_to :user
     has_many :items
     belongs_to :mall, optional: true
+
+    def format_name
+        return "#{fname} #{lname}"
+    end
+
+    def format_street_address
+        if unit_number.nil? || unit_number.empty?
+            return "#{street_address}"
+        else
+            return "#{unit_number} - #{street_address}"
+        end
+    end
+
+    def format_city_province_postal
+        return "#{city}, #{province}, #{postal}"
+    end
 end
