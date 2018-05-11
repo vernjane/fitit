305aa3f1607abfd78b7c5ee18dae927a32ff3774
vernjane <vern.jane@gmail.com>
2018-05-11 13:28:44 -0400

    Turn off mailer on development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,11 +29,11 @@ Rails.application.configure do
   end
 
   # Care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
-  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.perform_deliveries = false 
 
   config.action_mailer.perform_caching = false
 
